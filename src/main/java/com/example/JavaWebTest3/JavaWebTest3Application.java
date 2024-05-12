d96241d9902986ad2b91c8d4b345670b0f702e80
Ilia <ilcheg@yandex.ru>
2024-05-12 12:39:10 +0300

    Initial commit

---
+++ src/main/java/com/example/JavaWebTest3/JavaWebTest3Application.java
@@ -0,0 +1,12 @@
+package com.example.JavaWebTest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebTest3Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/JavaWebTest3/JavaWebTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaWebTest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebTest3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
